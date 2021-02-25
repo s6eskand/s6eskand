6b2ba07b6ba4c39f8ee0d5625e2715d630116d18
Sam <sam.eskandar00@hotmail.com>
2021-02-24 22:56:23 -0500

    update card

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -1,5 +1,14 @@
 export const WORK = [
-
+    {
+        title: "SWE Intern, Autonomous Vehicles",
+        description: <p>
+            Built gRPC messaging service in Java to send Autonomous Vehicle data to scenario simulation service on AWS.
+            <br/>
+            Developed UI in React to plan, organize, and map trip requests, display telemetry data streamed over HTTP/2,
+            and handle simulated scenarios.
+        </p>,
+        image: "https://i2.wp.com/www.geoawesomeness.com/wp-content/uploads/2017/09/Coding-Geospatial.jpg?resize=1152%2C768&ssl=1"
+    }
 ]
 
 export const PROJECTS = [
--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -6,15 +6,26 @@ import './Portfolio.css';
 // custom components
 import ExperienceCard from "../components/experience/ExperienceCard";
 
+// constants
+import {
+    WORK,
+    PROJECTS
+} from "../components/experience/experience";
+
 function Portfolio() {
 
     return(
         <div className="portfolio">
             <div className="container">
-                <ExperienceCard
-                    title={"SWE Intern, Autonomous Vehicles"}
-                    description={"test description this is lo"}
-                />
+                <mark className="title-text">Work Experience</mark> <br/> <br/>
+                {WORK.map(work => (
+                    <ExperienceCard
+                        title={work.title}
+                        description={work.description}
+                        image={work.image}
+                    />
+                ))}
+                <mark className="title-text">Personal Projects</mark> <br/> <br/>
             </div>
         </div>
     )
